676a06c8c23a6b183f7c2228eca9713466da9ea1
msc007 <alstjr5569@gmail.com>
2019-01-19 16:31:12 -0800

    loop template added

---
+++ loop_javascript/loop.js
@@ -0,0 +1,22 @@
+/* basic for loop:
+ */
+for(let i = 0; i < length; i++){
+}
+
+/* basic while loop:
+ */
+while(true){
+}
+
+/* for...of
+ * iterate iterable (array, string)
+ */
+for(let char of str){
+}
+
+/* for...in
+ * iterate over the properties of object (key, index)
+ */
+for (let index in str) {
+    console.log(`Index of ${str[index]}: ${index}`);
+}
