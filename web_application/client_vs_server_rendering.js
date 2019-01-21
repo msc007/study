ea1e7f0bd6c598c72ebbea9f8523603b5e2c7c4f
msc007 <alstjr5569@gmail.com>
2019-01-20 23:00:12 -0800

    web cheatsheet added

---
+++ web_application/client_vs_server_rendering.js
@@ -0,0 +1,15 @@
+/*
+
+Clinet Side:
+-Single Page Application(SPA)
+-HTML page rendered once at the beginning, then AJAX handles page update
+-Faster reload after initial rendering
+-More suitable for web-application
+
+Server Side:
+-Server Side Rendered(SSR)
+-HTML page render every time page needs update.
+-More suitable for Search Engine Optimization(SEO)
+
+
+*/
